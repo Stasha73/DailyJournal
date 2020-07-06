8c79ec3c6494c8aad186a8a1d1abf5b6ea3a2aff
Stacie <Stasharay05@gmail.com>
2020-07-05 20:33:14 -0500

    creating local git repository

---
+++ src/main/java/com/strProjects/DailyJornalApplication.java
@@ -0,0 +1,13 @@
+package com.strProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyJornalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyJornalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strProjects/DailyJornalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyJornalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
